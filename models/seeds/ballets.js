14cb839fb989ee011fce12159fe99e449f52273a
Hoskyfull <norberto.cardenales@gmail.com>
2022-06-11 22:06:36 -0600

    updated ballets seed file

--- models/seeds/ballets.js
+++ models/seeds/ballets.js
@@ -2,7 +2,7 @@
 // Import Dependencies
 ///////////////////////////////////////
 const mongoose = require("./connection");
-const Fruit = require('../balletmodel');
+const Ballets = require('../balletmodel');
 
 ///////////////////////////////////////////
 // Seed Code
@@ -14,87 +14,87 @@ const db = mongoose.connection;
 // Make sure code is not run till connected
 db.on("open", () => {
 
-const startBallet = [
-    {
-    name: 'Swan Lake',
-    composed: 'Pyotr Ilyich Tchaikovsky',
-    choreographed: 'Julius Reisinger',
-    premiered: 1877,
-},
-    {
-    name: 'The Nutcracker',
-    composed: 'Pyotr Ilyich Tchaikovsky',
-    choreographed: 'Marius Petipa and Lev Ivanov',
-    premiered: 1892,
-},
-    {
-    name: 'Giselle',
-    composed: 'Adolph Adam',
-    choreographed: 'Jean Coralli and Jules Perrot',
-    premiered: 1841,
-},
-    {
-    name: 'Romeo and Juliet',
-    composed: 'Sergei Prokofiev',
-    choreographed: 'Váña-Psota',
-    premiered: 1938,
-},
-    {
-    name: 'Dpn Quixote',
-    composed: 'Ludwig Minkus',
-    choreographed: 'Marius Petipa',
-    premiered: 1869,
-},
-    {
-    name: 'Cinderella',
-    composed: 'Sergei Prokofiev',
-    choreographed: 'Rostislav Zakharov',
-    premiered: 1945,
-},
-    {
-    name: 'La Bayadère',
-    composed: 'Ludwig Minkus',
-    choreographed: 'Marius Petipa',
-    premiered: 1877,
-},
-    {
-    name: 'Coppélia',
-    composed: 'Léo Delibes',
-    choreographed: 'Arthur Saint-Léon',
-    premiered: 1870,
-},
-    {
-    name: 'The Sleeping Beauty',
-    composed: 'Pyotr Ilyich Tchaikovsky',
-    choreographed: 'Marius Petipa',
-    premiered: 1890,
-},
-    {
-    name: 'La Sylphide',
-    composed: 'Jean-Madeleine Schneitzhoeffer',
-    choreographed: 'Marie Taglioni',
-    premiered: 1832,
-},
-    
-];
+    const startBallet = [
+        {
+            name: 'Swan Lake',
+            composed: 'Pyotr Ilyich Tchaikovsky',
+            choreographed: 'Julius Reisinger',
+            premiered: 1877,
+        },
+        {
+            name: 'The Nutcracker',
+            composed: 'Pyotr Ilyich Tchaikovsky',
+            choreographed: 'Marius Petipa and Lev Ivanov',
+            premiered: 1892,
+        },
+        {
+            name: 'Giselle',
+            composed: 'Adolph Adam',
+            choreographed: 'Jean Coralli and Jules Perrot',
+            premiered: 1841,
+        },
+        {
+            name: 'Romeo and Juliet',
+            composed: 'Sergei Prokofiev',
+            choreographed: 'Váña-Psota',
+            premiered: 1938,
+        },
+        {
+            name: 'Dpn Quixote',
+            composed: 'Ludwig Minkus',
+            choreographed: 'Marius Petipa',
+            premiered: 1869,
+        },
+        {
+            name: 'Cinderella',
+            composed: 'Sergei Prokofiev',
+            choreographed: 'Rostislav Zakharov',
+            premiered: 1945,
+        },
+        {
+            name: 'La Bayadère',
+            composed: 'Ludwig Minkus',
+            choreographed: 'Marius Petipa',
+            premiered: 1877,
+        },
+        {
+            name: 'Coppélia',
+            composed: 'Léo Delibes',
+            choreographed: 'Arthur Saint-Léon',
+            premiered: 1870,
+        },
+        {
+            name: 'The Sleeping Beauty',
+            composed: 'Pyotr Ilyich Tchaikovsky',
+            choreographed: 'Marius Petipa',
+            premiered: 1890,
+        },
+        {
+            name: 'La Sylphide',
+            composed: 'Jean-Madeleine Schneitzhoeffer',
+            choreographed: 'Marie Taglioni',
+            premiered: 1832,
+        },
 
- // Delete all companies
- Companies.deleteMany({})
- .then((deletedCompanies) => {
-     // add the starter companies
-     Companies.create(startCompany)
-         .then((newCompanies) => {
-             // log the new companies to confirm their creation
-             console.log(newCompanies);
-             db.close();
-         })
-         .catch((error) => {
-             console.log(error);
-             db.close();
-         });
- })
- .catch((error) => {
-     console.log(error);
-     db.close();
- });
+    ];
+
+    // Delete all ballets
+    Ballets.deleteMany({})
+        .then((deletedBallets) => {
+            // add the starter ballets
+            Ballets.create(startBallets)
+                .then((newBallets) => {
+                    // log the new ballets to confirm their creation
+                    console.log(newBallets);
+                    db.close();
+                })
+                .catch((error) => {
+                    console.log(error);
+                    db.close();
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+            db.close();
+        });
 });
