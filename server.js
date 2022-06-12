6199c04e3fe44f33057fda8a28ddce786bb43e3b
Hoskyfull <norberto.cardenales@gmail.com>
2022-06-11 22:22:48 -0600

    created ballets and companies liquid files

--- server.js
+++ server.js
@@ -5,27 +5,11 @@ require("dotenv").config(); // Load ENV Variables
 const express = require("express"); // import express
 const morgan = require("morgan"); //import morgan
 const methodOverride = require("method-override");
-const mongoose = require("mongoose");
 const path = require("path")
-
-/////////////////////////////////////////////
-// Database Connection
-/////////////////////////////////////////////
-// Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL;
-const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-// Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG);
-
-// Events for when connection opens/disconnects/errors
-mongoose.connection
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"))
-  .on("error", (error) => console.log(error));
+const FruitRouter = require('./controllers/fruits')
+const UserRouter = require("./controllers/user");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 
 /////////////////////////////////////////////////
@@ -40,6 +24,16 @@ app.use(morgan("tiny")); //logging
 app.use(methodOverride("_method")); // override for put and delete requests from forms
 app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use(express.static("public")); // serve files from public statically
+
+// middleware to setup session
+app.use(
+  session({
+    secret: process.env.SECRET,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+    saveUninitialized: true,
+    resave: false,
+  })
+);
 
 ////////////////////////////////////////////
 // Routes
