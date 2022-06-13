ee61181a4d5e2c17acc9d64a0523850316049330
Hoskyfull <norberto.cardenales@gmail.com>
2022-06-13 09:13:23 -0600

    updated secret module

--- controllers/companies.js
+++ controllers/companies.js
@@ -2,7 +2,7 @@
 // Import Dependencies
 ////////////////////////////////////////
 const express = require("express");
-const Fruit = require("../models/fruits");
+const Company = require("/Users/berto/Desktop/GA/projecttwo/Danceinf/Danceinf/models/companymodel.js");
 
 /////////////////////////////////////////
 // Create Route
@@ -28,12 +28,12 @@ router.use((req, res, next) => {
 
 // index route
 router.get("/", (req, res) => {
-    // find all the fruits
-    Fruit.find({ username: req.session.username })
+    // find all the companies
+    Company.find({ username: req.session.username })
         // render a template after they are found
-        .then((fruits) => {
-            console.log(fruits);
-            res.render("fruits/index.liquid", { fruits });
+        .then((companies) => {
+            console.log(companies);
+            res.render("/Users/berto/Desktop/GA/projecttwo/Danceinf/Danceinf/views/index.liquid", { companies });
         })
         // send error as json if they aren't
         .catch((error) => {
@@ -45,7 +45,7 @@ router.get("/", (req, res) => {
 
 // new route
 router.get("/new", (req, res) => {
-    res.render("fruits/new.liquid");
+    res.render("/Users/berto/Desktop/GA/projecttwo/Danceinf/Danceinf/views/index.liquid");
 });
 
 // create route
@@ -54,11 +54,11 @@ router.post("/", (req, res) => {
     req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
     // add username to req.body to track related user
     req.body.username = req.session.username;
-    // create the new fruit
-    Fruit.create(req.body)
-        .then((fruits) => {
+    // create the new companies
+    Company.create(req.body)
+        .then((companies) => {
             // redirect user to index page if successfully created item
-            res.redirect("/fruits");
+            res.redirect("/companies");
         })
         // send error as json
         .catch((error) => {
@@ -72,11 +72,11 @@ router.post("/", (req, res) => {
 router.get("/:id/edit", (req, res) => {
     // get the id from params
     const id = req.params.id;
-    // get the fruit from the database
-    Fruit.findById(id)
-        .then((fruit) => {
-            // render edit page and send fruit data
-            res.render("fruits/edit.liquid", { fruit });
+    // get the companies from the database
+    Company.findById(id)
+        .then((companies) => {
+            // render edit page and send companies data
+            res.render("/Users/berto/Desktop/GA/projecttwo/Danceinf/Danceinf/views/companies/edit.liquid", { companies });
         })
         // send error as json
         .catch((error) => {
@@ -91,11 +91,11 @@ router.put("/:id", (req, res) => {
     const id = req.params.id;
     // check if the readyToEat property should be true or false
     req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
-    // update the fruit
-    Fruit.findByIdAndUpdate(id, req.body, { new: true })
-        .then((fruit) => {
+    // update the company
+    Company.findByIdAndUpdate(id, req.body, { new: true })
+        .then((companies) => {
             // redirect to main page after updating
-            res.redirect("/fruits");
+            res.redirect("/companies");
         })
         // send error as json
         .catch((error) => {
@@ -107,11 +107,11 @@ router.put("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
     // get the id from params
     const id = req.params.id;
-    // delete the fruit
-    Fruit.findByIdAndRemove(id)
-        .then((fruit) => {
+    // delete the company
+    Companies.findByIdAndRemove(id)
+        .then((companies) => {
             // redirect to main page after deleting
-            res.redirect("/fruits");
+            res.redirect("/companies");
         })
         // send error as json
         .catch((error) => {
@@ -125,12 +125,12 @@ router.get("/:id", (req, res) => {
     // get the id from params
     const id = req.params.id;
 
-    // find the particular fruit from the database
-    Fruit.findById(id)
-        .then((fruit) => {
-            console.log(fruit);
+    // find the particular company from the database
+    Companies.findById(id)
+        .then((companies) => {
+            console.log(companies);
             // render the template with the data from the database
-            res.render("fruits/show.liquid", { fruit });
+            res.render("/Users/berto/Desktop/GA/projecttwo/Danceinf/Danceinf/views/companies/show.liquid", { companies });
         })
         .catch((error) => {
             console.log(error);
