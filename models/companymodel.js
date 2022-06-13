98336f294ba6797bb831b58379615dec025d49d4
Hoskyfull <norberto.cardenales@gmail.com>
2022-06-13 14:50:00 -0600

    modified several liquid files for ballets model

--- models/companymodel.js
+++ models/companymodel.js
@@ -1,28 +1,28 @@
-const mongoose = require('./connection')
+// const mongoose = require('./connection')
 
-////////////////////////////////////////////////
-// Our Models
-////////////////////////////////////////////////
-// pull schema and model from mongoose
-const { Schema, model } = mongoose;
+// ////////////////////////////////////////////////
+// // Our Models
+// ////////////////////////////////////////////////
+// // pull schema and model from mongoose
+// const { Schema, model } = mongoose;
 
-// make Dance Company schema
-const danceCoSchema = new Schema({
-    name: String,
-    location: String,
-    founded: Number,
-    rank: Number,
-    rankyear: Number,
-    //ballets: [object.ref]
-    ///mongoose populate method read about it
-    //to be added in either index or show through the get route
-  });
+// // make Dance Company schema
+// const danceCoSchema = new Schema({
+//     name: String,
+//     location: String,
+//     founded: Number,
+//     rank: Number,
+//     rankyear: Number,
+//     //ballets: [object.ref]
+//     ///mongoose populate method read about it
+//     //to be added in either index or show through the get route
+//   });
   
-  // make Dance Company model
-  const Company = model("Company", danceCoSchema);
+//   // make Dance Company model
+//   const Company = model("Company", danceCoSchema);
 
 
-  ///////////////////////////////////////////////////
-// Export Model
-///////////////////////////////////////////////////
-module.exports = Company;
+//   ///////////////////////////////////////////////////
+// // Export Model
+// ///////////////////////////////////////////////////
+// module.exports = Company;
