2e6feb4ddc737c8f2d857c1ed56cc3fbeefe0ec6
Hoskyfull <norberto.cardenales@gmail.com>
2022-06-16 11:19:15 -0600

    heroku push

--- models/connection.js
+++ models/connection.js
@@ -9,18 +9,18 @@ const mongoose = require("mongoose");
 // Database Connection
 /////////////////////////////////////////////
 // Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
 
 // Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG);
+mongoose.connect(MONGODB_URI, CONFIG);
 
 // Events for when connection opens/disconnects/errors
 mongoose.connection
-    .on("open", () => console.log("Connected to Mongoose"))
+    .on("open", () => console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`))
     .on("close", () => console.log("Disconnected from Mongoose"))
     .on("error", (error) => console.log(error));
 
